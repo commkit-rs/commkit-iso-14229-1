33dd665a2e308bd4d431b0d002f82ea011fc4a47
Nick Daria <contact@nickdaria.com>
2026-09-23 01:55:33 -0500

    Add some constants

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,9 @@ pub use service::UdsService;
 pub use service_entry::UdsServiceEntry;
 pub use sid::UdsSid;
 pub use subfunction::UdsSubfunction;
+
+/// UDS protocol version, packed as `major << 24 | minor << 16 | revision << 8 | b4` as defined in the spec
+pub const UDS_PROTOCOL_VERSION: u32 = (2 << 24) | (0 << 16) | (0 << 8) | 0;
+
+/// Service byte offset for positive acknowledgement
+pub const UDS_PROTOCOL_POSITIVE_RESPONSE_SID_OFFSET: u8 = 0x40;
